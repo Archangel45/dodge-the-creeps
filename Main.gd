26d66577e3bb8f147d3f72903ca7976905efed9a
Franco Lorenzo Arive <angelslashes@gmail.com>
2018-02-08 12:34:41 +0800

    Added particles, music and a baby blue background.

--- Main.gd
+++ Main.gd
@@ -9,6 +9,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _ready():
 	randomize()
@@ -18,6 +19,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
